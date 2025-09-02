3d7395862788c95bfce9cd424dd64bf96b0e2999
yoonxjoong <yhj8372@naver.com>
2025-09-02 16:25:03 +0900

    feat: GitHub Actions를 샘플 코드 작성

--- src/main/java/com/yoonxjoon/api/ApiApplication.java
+++ src/main/java/com/yoonxjoon/api/ApiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApiApplication {
 
+    // GitHub action 변경 테스트
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
     }
